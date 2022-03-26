fe448b2aa49e83abdbc61d97ccd5bbc98b9b2b6b
Andreas Leopold <andreasleopold97@hotmail.com>
2022-03-26 17:18:22 +0000

    fix: Already connected exception
    git-tfs-id: [https://dev.azure.com/code-a-software]$/home/Home;C707

--- API/Home.API/Controllers/CommunicationController.cs
+++ API/Home.API/Controllers/CommunicationController.cs
@@ -25,17 +25,36 @@ namespace Home.API.Controllers
         public IActionResult Login([FromBody] Client client)
         {
             if (client == null || !client.IsRealClient)
-                return NotFound(AnswerExtensions.Fail("Invalid client data"));
+                return NotFound(AnswerExtensions.Fail("Invalid client data!"));
 
             if (!Program.Clients.Any(p => p.ID == client.ID))
             {
-                Program.Clients.Add(client);
-                Program.EventQueues.Add(new Data.Events.EventQueue() { ClientID = client.ID, LastClientRequest = DateTime.Now });
+                lock (Program.Clients)
+                {
+                    Program.Clients.Add(client);
+                }
+
+                lock (Program.EventQueues)
+                {
+                    Program.EventQueues.Add(new Data.Events.EventQueue() { ClientID = client.ID, LastClientRequest = DateTime.Now });
+                }
 
                 _logger.LogInformation($"Client {client} has just logged in!");
             }
             else
-                return BadRequest(AnswerExtensions.Fail("Already logged in"));
+            {
+                _logger.LogWarning($"Client {client} was already logged in!");
+
+                // But updated LastClientRequest
+                lock (Program.EventQueues)
+                {
+                    var queue = Program.EventQueues.Where(d => d.ClientID == client.ID).FirstOrDefault();
+                    if (queue != null)
+                        queue.LastClientRequest = DateTime.Now;
+                }
+
+                // return BadRequest(AnswerExtensions.Fail("Already logged in"));
+            }
 
             return Ok(AnswerExtensions.Success(Program.Devices));
         }
